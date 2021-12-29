2cd4b18587a4a029bf391babc0bff0684ab81314
AleksaStarcevic <aleksastarcevic1@gmail.com>
2021-12-29 02:03:46 +0100

    Uradjena autentifikacija

--- database/migrations/2021_12_20_181949_add_column_address_in_users_table.php
+++ database/migrations/2021_12_20_181949_add_column_address_in_users_table.php
@@ -14,7 +14,7 @@ class AddColumnAddressInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('address');
+            $table->string('address')->default('Beograd');
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,4 +29,17 @@ Route::get('/ads/{id}', [AdController::class, 'show']);
 Route::post('/ads', [AdController::class, 'store']);
 Route::delete('/ads/{id}', [AdController::class, 'destroy']);
 Route::patch('/ads/{id}', [AdController::class, 'update']);
-Route::resource('ads', AdController::class);
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+    Route::resource('ads', AdController::class)->only(['update', 'store', 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::resource('ads', AdController::class)->only(['index']);
